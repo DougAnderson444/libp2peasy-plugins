c42853bc4607b49ef371744b84c415798efad818
Doug A <douganderson444@gmail.com>
2023-06-18 10:23:05 -0300

    rm dead

--- src/routes/ipns-pubsub/+page.js
+++ src/routes/ipns-pubsub/+page.js
@@ -1,12 +1,3 @@
-// needs to provide api for
-// prnt, publish, subscribe, unsubscribe
-export const ipnsPubsubImports = {
-	prnt: () => console.log('Printing from Host'),
-	publish: () => console.log('Publishing from Host'),
-	subscribe: () => console.log('Subscribing from Host'),
-	unsubscribe: () => console.log('Unsubscribing from Host')
-};
-
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
